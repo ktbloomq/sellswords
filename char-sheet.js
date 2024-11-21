55a92ade6daa7ef17d453cdf47e3dc7479e307ef
Kevin Bloomquist <ktb@bloomquist.com>
2024-11-20 21:45:51 -0800

    field ids, initial pools layout

---
+++ char-sheet.js
@@ -0,0 +1,49 @@
+window.onload = function() {
+    // document.getElementById("physique-bonus").textContent=0;
+    // document.getElementById("physique-injury").textContent=0;
+    // document.getElementById("physique-specialization").textContent=0;
+    // document.getElementById("intimidation-bonus").textContent=0;
+    // document.getElementById("intimidation-injury").textContent=0;
+    // document.getElementById("strength-bonus").textContent=0;
+    // document.getElementById("strength-injury").textContent=0;
+
+    // document.getElementById("precision-bonus").textContent=0;
+    // document.getElementById("precision-injury").textContent=0;
+    // document.getElementById("precision-specialization").textContent=0;
+    // document.getElementById("pickpocket-bonus").textContent=0;
+    // document.getElementById("pickpocket-injury").textContent=0;
+    // document.getElementById("hide-bonus").textContent=0;
+    // document.getElementById("hide-injury").textContent=0;
+
+    // document.getElementById("intuition-bonus").textContent=0;
+    // document.getElementById("intuition-injury").textContent=0;
+    // document.getElementById("intuition-specialization").textContent=0;
+    // document.getElementById("blend-bonus").textContent=0;
+    // document.getElementById("blend-injury").textContent=0;
+    // document.getElementById("diplomacy-bonus").textContent=0;
+    // document.getElementById("diplomacy-injury").textContent=0;
+
+    // document.getElementById("smarts-bonus").textContent=0;
+    // document.getElementById("smarts-injury").textContent=0;
+    // document.getElementById("smarts-specialization").textContent=0;
+    // document.getElementById("focus-bonus").textContent=0;
+    // document.getElementById("focus-injury").textContent=0;
+    // document.getElementById("tinkering-bonus").textContent=0;
+    // document.getElementById("tinkering-injury").textContent=0;
+
+    // document.getElementById("wit-bonus").textContent=0;
+    // document.getElementById("wit-injury").textContent=0;
+    // document.getElementById("wit-specialization").textContent=0;
+    // document.getElementById("business-bonus").textContent=0;
+    // document.getElementById("business-injury").textContent=0;
+    // document.getElementById("bluff-bonus").textContent=0;
+    // document.getElementById("bluff-injury").textContent=0;
+
+    // document.getElementById("soul-bonus").textContent=0;
+    // document.getElementById("soul-injury").textContent=0;
+    // document.getElementById("soul-specialization").textContent=0;
+    // document.getElementById("read-person-bonus").textContent=0;
+    // document.getElementById("read-person-injury").textContent=0;
+    // document.getElementById("alchemy-bonus").textContent=0;
+    // document.getElementById("alchemy-injury").textContent=0;
+}
