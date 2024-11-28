d832f54cfb5674043636eea048499ca78f4a14c0
Kevin Bloomquist <ktb@bloomquist.com>
2024-11-27 21:56:15 -0800

    calculate skills

--- char-sheet.js
+++ char-sheet.js
@@ -22,50 +22,51 @@ let alchemyElement;
 let player;
 
 function updateElements() {
-    physiqueElement.textContent = player.attributes.physical.physique.raw;
-    intimidationElement.textContent = player.attributes.physical.physique.intimidation;
-    strengthElement.textContent = player.attributes.physical.physique.strength;
-    precisionElement.textContent = player.attributes.physical.precision.raw
-    pickpocketElement.textContent = player.attributes.physical.precision.pickpocket;
-    hideElement.textContent = player.attributes.physical.precision.hide;
-    intuitionElement.textContent = player.attributes.mental.intuition.raw
-    blendElement.textContent = player.attributes.mental.intuition.blend;
-    diplomacyElement.textContent = player.attributes.mental.intuition.diplomacy;
-    smartsElement.textContent = player.attributes.mental.smarts.raw
-    focusElement.textContent = player.attributes.mental.smarts.focus;
-    tinkeringElement.textContent = player.attributes.mental.smarts.tinkering;
-    witElement.textContent = player.attributes.spiritual.wit.raw
-    businessElement.textContent = player.attributes.spiritual.wit.business;
-    bluffElement.textContent = player.attributes.spiritual.wit.bluff;
-    soulElement.textContent = player.attributes.spiritual.soul.raw
-    readPersonElement.textContent = player.attributes.spiritual.soul.readPerson;
-    alchemyElement.textContent = player.attributes.spiritual.soul.alchemy;
+	physiqueElement.textContent = player.attributes.physical.physique.raw;
+	intimidationElement.textContent = player.attributes.physical.physique.intimidation;
+	strengthElement.textContent = player.attributes.physical.physique.strength;
+	precisionElement.textContent = player.attributes.physical.precision.raw
+	pickpocketElement.textContent = player.attributes.physical.precision.pickpocket;
+	hideElement.textContent = player.attributes.physical.precision.hide;
+	intuitionElement.textContent = player.attributes.mental.intuition.raw
+	blendElement.textContent = player.attributes.mental.intuition.blend;
+	diplomacyElement.textContent = player.attributes.mental.intuition.diplomacy;
+	smartsElement.textContent = player.attributes.mental.smarts.raw
+	focusElement.textContent = player.attributes.mental.smarts.focus;
+	tinkeringElement.textContent = player.attributes.mental.smarts.tinkering;
+	witElement.textContent = player.attributes.spiritual.wit.raw
+	businessElement.textContent = player.attributes.spiritual.wit.business;
+	bluffElement.textContent = player.attributes.spiritual.wit.bluff;
+	soulElement.textContent = player.attributes.spiritual.soul.raw
+	readPersonElement.textContent = player.attributes.spiritual.soul.readPerson;
+	alchemyElement.textContent = player.attributes.spiritual.soul.alchemy;
 }
 
 window.onload = function() {
-    console.log("test");
-    player = new Player();
-    let race = new Races.Elf();
-    player.applyRace(race);
-    physiqueElement = document.getElementById("physique-bonus");
-    intimidationElement = document.getElementById("intimidation-bonus");
-    strengthElement = document.getElementById("strength-bonus");
-    precisionElement = document.getElementById("precision-bonus");
-    pickpocketElement = document.getElementById("pickpocket-bonus");
-    hideElement = document.getElementById("hide-bonus");
-    intuitionElement = document.getElementById("intuition-bonus");
-    blendElement = document.getElementById("blend-bonus");
-    diplomacyElement = document.getElementById("diplomacy-bonus");
-    smartsElement = document.getElementById("smarts-bonus");
-    focusElement = document.getElementById("focus-bonus");
-    tinkeringElement = document.getElementById("tinkering-bonus");
-    witElement = document.getElementById("wit-bonus");
-    businessElement = document.getElementById("business-bonus");
-    bluffElement = document.getElementById("bluff-bonus");
-    soulElement = document.getElementById("soul-bonus");
-    readPersonElement = document.getElementById("read-person-bonus");
-    alchemyElement = document.getElementById("alchemy-bonus");
-
-    updateElements();
+	console.log("test");
+	
+	physiqueElement = document.getElementById("physique-bonus");
+	intimidationElement = document.getElementById("intimidation-bonus");
+	strengthElement = document.getElementById("strength-bonus");
+	precisionElement = document.getElementById("precision-bonus");
+	pickpocketElement = document.getElementById("pickpocket-bonus");
+	hideElement = document.getElementById("hide-bonus");
+	intuitionElement = document.getElementById("intuition-bonus");
+	blendElement = document.getElementById("blend-bonus");
+	diplomacyElement = document.getElementById("diplomacy-bonus");
+	smartsElement = document.getElementById("smarts-bonus");
+	focusElement = document.getElementById("focus-bonus");
+	tinkeringElement = document.getElementById("tinkering-bonus");
+	witElement = document.getElementById("wit-bonus");
+	businessElement = document.getElementById("business-bonus");
+	bluffElement = document.getElementById("bluff-bonus");
+	soulElement = document.getElementById("soul-bonus");
+	readPersonElement = document.getElementById("read-person-bonus");
+	alchemyElement = document.getElementById("alchemy-bonus");
+	
+	player = new Player();
+	let race = new Races.Elf();
+	player.calcSkills(race);
+	updateElements();
 
 }
--- player.js
+++ player.js
@@ -1,63 +1,79 @@
 import * as Races from "./races.js";
 export class Player {
-    race;
-    attributes = {
-        "physical": {
-            "physique": {
-                "raw": -4,
-                "intimidation": -4,
-                "strength": -4
-            },
-            "precision": {
-                "raw": -4,
-                "pickpocket": -4,
-                "hide": -4
-            }
-        },
-        "mental": {
-            "intuition": {
-                "raw": -4,
-                "blend": -4,
-                "diplomacy": -4
-            },
-            "smarts": {
-                "raw": -4,
-                "focus": -4,
-                "tinkering": -4
-            }
-        },
-        "spiritual": {
-            "wit": {
-                "raw": -4,
-                "business": -4,
-                "bluff": -4
-            },
-            "soul": {
-                "raw": -4,
-                "readPerson": -4,
-                "alchemy": -4
-            }
-        }
-    }
-    level;
-    health = {
-        "current": 0,
-        "max": 0,
-    };
-    energy = {
-        "current": 0,
-        "max": 0
-    };
-    mana = {
-        "current": 0,
-        "max": 0
-    };
-    freeActions;
-    actions;
-    actionDice;
-    actionDiceType;
-    applyRace(r) {
-        this.race = r;
-        this.attributes = this.race.applyAttributeModifiers(this.attributes);
-    }
+	race;
+	attributes = {
+		physical: {
+			physique: {
+				raw: -4,
+				intimidation: -4,
+				strength: -4
+			},
+			precision: {
+				raw: -4,
+				pickpocket: -4,
+				hide: -4
+			}
+		},
+		mental: {
+			intuition: {
+				raw: -4,
+				blend: -4,
+				diplomacy: -4
+			},
+			smarts: {
+				raw: -4,
+				focus: -4,
+				tinkering: -4
+			}
+		},
+		spiritual: {
+			wit: {
+				raw: -4,
+				business: -4,
+				bluff: -4
+			},
+			soul: {
+				raw: -4,
+				readPerson: -4,
+				alchemy: -4
+			}
+		}
+	}
+	level;
+	health = {
+		current: 0,
+		max: 0,
+	};
+	energy = {
+		current: 0,
+		max: 0
+	};
+	mana = {
+		current: 0,
+		max: 0
+	};
+	freeActions;
+	actions;
+	actionDice;
+	actionDiceType;
+	applyRace(r) {
+		this.race = r;
+		this.attributes = this.race.applyAttributeModifiers(this.attributes);
+	}
+	calcSkills(r) {
+		Object.keys(this.attributes).forEach((areaKey) => {
+			Object.keys(this.attributes[areaKey]).forEach((attrKey) => {
+				let skills = this.attributes[areaKey][attrKey];
+				Object.entries(skills).slice(1).forEach(([sKey, sVal]) => {
+					skills[sKey] = skills.raw*5;
+					if(skills.raw > 0) {
+						skills[sKey]  = Math.ceil(skills[sKey]/2)+3
+					}
+				});
+			});
+		});
+
+		this.applyRace(r);
+		// TODO: apply boons
+	}
 }
